04738f26492d23e6a8b6faa5542eb471359ab091
Stephanie La Raja <stephanielaraja@gmail.com>
2021-02-11 06:35:52 -0500

    commit tree 5011e1ea127ada2230b9c68146fe2d0b877aab6b

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,17 @@
 require 'pry'
-
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods 
+  include Paramable
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
+  end 
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
@@ -17,14 +19,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -35,8 +29,5 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  
 end
